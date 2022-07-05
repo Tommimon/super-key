0b523cb694289716fbfe1991ee2a1e28515eb7ca
tommimon <37435103+tommimon@users.noreply.github.com>
2022-07-05 17:56:07 +0200

    Added build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+# compile schemas
+glib-compile-schemas schemas/
+
+# find version
+while read line
+do
+    if echo "$line" | grep -q '"version":'
+    then
+        version=${line:11:${#line}-11};
+    fi
+done < metadata.json
+
+# create .zip
+mkdir output
+zip output/super-key@tommimon.github.com.v"${version}".shell-extension.zip ./*.js README.md LICENSE metadata.json -r schemas
